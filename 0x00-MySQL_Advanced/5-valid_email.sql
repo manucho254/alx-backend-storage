1e38deaab6a05b7f3b16375ae69f148ae491d54f
manucho254 <manuchoadero@gmail.com>
2024-01-19 20:19:22 +0000

    fixes

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,10 +5,12 @@ DELIMITER $$
 CREATE TRIGGER before_update_user
 BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
-    IF NEW.email != OLD.email THEN
-	SET NEW.valid_email = 0;
-    ELSE
-	SET NEW.valid_email = 1;
+    IF NEW.email <> OLD.email THEN
+	IF OLD.valid_email == 0 THEN
+	  SET NEW.valid_email = 1;
+        ELSE
+          SET NEW.valid_email = 0;
+        END IF;
     END IF;
 END$$
 
