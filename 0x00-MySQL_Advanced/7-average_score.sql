2475d94726a035f7a8ebae624c0a915d06cd1ac6
manucho254 <manuchoadero@gmail.com>
2024-01-19 22:46:23 +0000

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,9 +5,11 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-   SELECT AVG(corrections.score) INTO @user_average
-   FROM corrections WHERE user_id = user_id;
+   SELECT AVG(corrections.score) INTO @get_avg
+   FROM users INNER JOIN corrections
+   ON corrections.user_id = users.id
+   WHERE users.id = user_id;
 
-   UPDATE users SET average_score = @user_average WHERE id = user_id;
+   UPDATE users SET average_score = @get_avg WHERE id = user_id;
 END//
 DELIMITER ;
