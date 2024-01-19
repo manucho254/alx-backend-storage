217eb86160b12c293ee258da0bb93b86f2671c31
manucho254 <manuchoadero@gmail.com>
2024-01-19 18:38:50 +0000

    added regex

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,7 +6,9 @@ CREATE TRIGGER before_update_user
 BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
     IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = !NEW.valid_email;
+	IF NEW.email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,4}$' THEN
+             SET NEW.valid_email = !NEW.valid_email;
+        END IF;
     END IF;
 END$$
 
