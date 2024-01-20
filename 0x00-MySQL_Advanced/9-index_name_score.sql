3ae5f449c6575213913ca347387691fce6c3e57e
manucho254 <manuchoadero@gmail.com>
2024-01-20 01:02:39 +0000

    Safe divide

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,13 +1,5 @@
 -- SQL script that creates an index idx_name_first_score
 -- on the table names and the first letter of name and the score.
 
--- Add a new column to store the first value
-ALTER TABLE names
-ADD COLUMN first_val INT;
-
--- Update the new column with the first value of your_column
-UPDATE names
-SET first_val = CAST(SUBSTRING(CAST(score AS CHAR), 1, 1) AS UNSIGNED);
-
 -- Create an index on the new column
-CREATE INDEX idx_name_first_score ON names (name(1), first_val);
+CREATE INDEX idx_name_first_score ON names (name(1), LEFT(CAST(score AS CHAR), 1));
