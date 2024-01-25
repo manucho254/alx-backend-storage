09da6ce6fa488c161c0c86010f402418ac1438e4
manucho254 <manuchoadero@gmail.com>
2024-01-25 00:52:48 +0000

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -23,10 +23,10 @@ def cache_result(func: Callable) -> Any:
         url = args[0]
         cached_page = client.get(url)
 
-        client.incr("count:{}".format(url))
         # send request again to url if no cache is found
         # an update info else get cached data
         if not cached_page:
+            client.incr("count:{}".format(url))
             return_val = func(*args, **kwargs)
             client.setex(url, timedelta(seconds=10), value=return_val)
             return return_val
