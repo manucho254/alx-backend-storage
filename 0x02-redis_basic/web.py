843525735f126a8ad452722334a05d613e8025fd
manucho254 <manuchoadero@gmail.com>
2024-01-25 00:50:04 +0000

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,7 +14,6 @@ client = redis.Redis()  # new redis instance
 def cache_result(func: Callable) -> Any:
     """ decorator function
     """
-    key = func.__qualname__
 
     @wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
@@ -22,14 +21,14 @@ def cache_result(func: Callable) -> Any:
             responses that expire in 10 seconds
         """
         url = args[0]
-        cached_page = client.get(key)
+        cached_page = client.get(url)
 
         client.incr("count:{}".format(url))
         # send request again to url if no cache is found
         # an update info else get cached data
         if not cached_page:
             return_val = func(*args, **kwargs)
-            client.setex(key, timedelta(seconds=10), value=return_val)
+            client.setex(url, timedelta(seconds=10), value=return_val)
             return return_val
 
         return client.get(key).decode("utf-8")
